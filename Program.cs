382f38d287bd85c8cbb1bc7f3b30552df3c116e9
Pavan Kumar Goud Pabba <pabba1@usf.edu>
2024-02-28 11:27:57 -0500

    Comments updated

--- Program.cs
+++ Program.cs
@@ -1,35 +1,47 @@
-﻿int[] selection_sort_numbers = { 5, 72, 3, 14, 7, 1 };
-Console.WriteLine("Unsorted Array");
-for (int i = 0; i < selection_sort_numbers.Length; i++)
-{
-    Console.Write("  " + selection_sort_numbers[i]);
-}
-Console.WriteLine("");
-Console.WriteLine("array after the execution of the Selection Sort Algorithm: ");
-Selection_Sort(selection_sort_numbers);
-Console.WriteLine("");
-static void Selection_Sort(int[] ss_arr)
-{
+﻿/*
+Author: Pavan Kumar Pabba
+This C# Console Application code demonstrates the Selection Sort.
+No input is required for the script. 
+It will execute without input against two pre-populated arrays. 
+*/
 
-    int min_position;
-    int temp;
-
-    for (int i = 0; i < ss_arr.Length; i++)
+using System;
+class Program
+{
+    static void Main(string[] args)
     {
-        min_position = i;
-        for (int x = i + 1; x < ss_arr.Length; x++)
+        int[] arr = { 5, 72, 3, 14, 7, 1 };  // unsorted list of elements
+        Console.WriteLine("Unsorted array:");
+        PrintArray(arr);
+        SelectionSort(arr);
+        Console.WriteLine("\nAfter Execution Selection sort the Sorted array is :");
+        PrintArray(arr);
+    }
+    static void SelectionSort(int[] arr)
+{
+    int n = arr.Length; // Get the length of the array
+    for (int i = 0; i < n - 1; i++) // Loop through the array from index 0 to n-2
+    {
+        int minIndex = i; // Assume the current index as the minimum index
+        for (int j = i + 1; j < n; j++) // Loop through the unsorted part of the array
         {
-            if (ss_arr[x] < ss_arr[min_position])
+            if (arr[j] < arr[minIndex]) // If we find a smaller element
             {
-                min_position = x;
+                minIndex = j; // Update the minimum index
             }
         }
-        if (min_position != i)
+        // Swap arr[i] and arr[minIndex]
+        int temp = arr[i]; // Store the current element in a temporary variable
+        arr[i] = arr[minIndex]; // Replace the current element with the minimum element found
+        arr[minIndex] = temp; // Place the current element in the position of the minimum element
+    }
+}
+    static void PrintArray(int[] arr)
+    {
+        foreach (var num in arr)
         {
-            temp = ss_arr[i];
-            ss_arr[i] = ss_arr[min_position];
-            ss_arr[min_position] = temp;
+            Console.Write(num + " ");
         }
-        Console.Write("  " + ss_arr[i]);
+        Console.WriteLine();
     }
 }
